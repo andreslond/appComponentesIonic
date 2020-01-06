c34fdf2f4a2f0d209af95b573d28a1f374997be2
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-01-06 14:29:32 -0500

    Clase 45: Ion Alert e Ion Avatar

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo = 'Alert Page';
+
+  constructor(
+    public alertController: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Cancel: blah');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Botón Ok');
+          }
+        }
+      ]
+    }
+    );
+
+    await alert.present();
+  }
+
+  async presentPrompt() {
+    const prompt = await this.alertController.create({
+      header: 'Input',
+      subHeader: 'Cambiar nombre',
+      inputs: [
+        {
+          name: 'newName',
+          type: 'text',
+          placeholder: 'Nombre'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            this.titulo = data.newName;
+          }
+        }
+      ]
+    }
+    );
+
+    await prompt.present();
   }
 
 }
